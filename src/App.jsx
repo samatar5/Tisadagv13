9e3037f9c32aac09eaeed886ec93e43b2ae0e969
Samatar5 <samatar5mohamed@gmail.com>
2023-02-28 11:12:48 +0100

    displayed products

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,19 @@
+import { Routes, Route, Link } from "react-router-dom";
+import Home from "./views/Home";
+import Products from "./views/Products";
+
 function App() {
-  return <div className="App">hej</div>;
+  return (
+    <div className="App">
+      <h1>Min Webshop</h1>
+      <Link to="/">home</Link>
+      <Link to="/products">products</Link>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/products" element={<Products />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
